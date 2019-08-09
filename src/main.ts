53bcb6d2af6c3c6a947a06f38234568818044fb9
hackarias <zack.gustavsson@gmail.com>
2019-08-09 21:45:48 +0200

    Add task.service tests

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,10 @@ async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
   const serverConfig = config.get('server');
-  console.log(serverConfig);
+
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
 
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
